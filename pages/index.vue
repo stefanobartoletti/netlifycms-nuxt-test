44bb0908f14047130438b4f57b9961ff685b3baf
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-03-02 19:36:14 +0100

    form test

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="main text-center flex flex-col items-center justify-center">
+  <div class="flex flex-col items-center justify-center text-center main">
     <!-- <logo /> -->
     <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
     <h2 class="subtitle">Boilerplate</h2>
+
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label
+          >Your Role:
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label
+        >
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </div>
 </template>
 
@@ -15,8 +39,12 @@ export default {
   // },
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      script: [
+        {
+          src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
+        },
+      ],
     }
-  }
+  },
 }
 </script>
